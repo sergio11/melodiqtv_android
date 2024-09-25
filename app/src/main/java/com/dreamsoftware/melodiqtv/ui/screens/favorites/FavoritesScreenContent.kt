e145d1832746591a6b426c0f73efb79eeb14611c
Sergio Sánchez <sss4esob@gmail.com>
2024-09-25 20:34:07 +0200

    Implement several improvements in favorite screen

--- app/src/main/java/com/dreamsoftware/melodiqtv/ui/screens/favorites/FavoritesScreenContent.kt
+++ app/src/main/java/com/dreamsoftware/melodiqtv/ui/screens/favorites/FavoritesScreenContent.kt
@@ -25,19 +25,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.tv.material3.MaterialTheme
 import coil.compose.AsyncImage
-import com.dreamsoftware.melodiqtv.R
-import com.dreamsoftware.melodiqtv.ui.theme.onSurface
-import com.dreamsoftware.melodiqtv.ui.theme.popupShadow
-import com.dreamsoftware.melodiqtv.ui.theme.surfaceContainerHigh
-import com.dreamsoftware.melodiqtv.ui.theme.surfaceVariant
 import com.dreamsoftware.fudge.component.FudgeTvButton
 import com.dreamsoftware.fudge.component.FudgeTvButtonStyleTypeEnum
 import com.dreamsoftware.fudge.component.FudgeTvButtonTypeEnum
@@ -50,8 +44,10 @@ import com.dreamsoftware.fudge.component.FudgeTvText
 import com.dreamsoftware.fudge.component.FudgeTvTextTypeEnum
 import com.dreamsoftware.fudge.utils.conditional
 import com.dreamsoftware.fudge.utils.shadowBox
+import com.dreamsoftware.melodiqtv.R
 import com.dreamsoftware.melodiqtv.domain.model.SongBO
-import com.dreamsoftware.melodiqtv.ui.utils.formatDuration
+import com.dreamsoftware.melodiqtv.ui.theme.popupShadow
+import com.dreamsoftware.melodiqtv.ui.theme.surfaceContainerHigh
 import com.dreamsoftware.melodiqtv.ui.utils.formatSubtitle
 
 @Composable
@@ -133,91 +129,95 @@ private fun SongDetailsPopup(
     onRemoveFromFavorites: (id: String) -> Unit,
     onBackPressed: () -> Unit
 ) {
-    Dialog(onDismissRequest = onBackPressed) {
-        FudgeTvFocusRequester { focusRequester ->
-            Box(
-                modifier = Modifier
-                    .background(surfaceContainerHigh, RoundedCornerShape(16.dp))
-                    .fillMaxWidth(0.75f)
-                    .fillMaxHeight(0.8f)
-                    .shadowBox(
-                        color = popupShadow,
-                        blurRadius = 40.dp,
-                        offset = DpOffset(0.dp, 8.dp)
-                    )
-                    .clip(RoundedCornerShape(16.dp))
-            ) {
-                AsyncImage(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight(0.5f)
-                        .alpha(0.88f),
-                    model = song.imageUrl,
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop
-                )
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(surfaceVariant.copy(alpha = 0.35f))
-                        .padding(horizontal = 24.dp),
-                    horizontalAlignment = Alignment.Start
-                ) {
-                    Spacer(modifier = Modifier.fillMaxHeight(0.35f))
-                    FudgeTvText(
-                        modifier = Modifier.padding(bottom = 8.dp),
-                        type = FudgeTvTextTypeEnum.HEADLINE_SMALL,
-                        textColor = onSurface,
-                        textAlign = TextAlign.Justify,
-                        titleText = song.title,
-                        overflow = TextOverflow.Ellipsis,
-                    )
-                    Row(
+    with(MaterialTheme.colorScheme) {
+        with(song) {
+            Dialog(onDismissRequest = onBackPressed) {
+                FudgeTvFocusRequester { focusRequester ->
+                    Box(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(bottom = 8.dp),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Start
+                            .background(surfaceContainerHigh, RoundedCornerShape(16.dp))
+                            .fillMaxWidth(0.75f)
+                            .fillMaxHeight(0.8f)
+                            .shadowBox(
+                                color = popupShadow,
+                                blurRadius = 40.dp,
+                                offset = DpOffset(0.dp, 8.dp)
+                            )
+                            .clip(RoundedCornerShape(16.dp))
                     ) {
-                        FudgeTvText(
-                            modifier = Modifier,
-                            type = FudgeTvTextTypeEnum.LABEL_MEDIUM,
-                            titleText = song.formatSubtitle(),
-                            textColor = onSurface,
-                            overflow = TextOverflow.Ellipsis,
-                            softWrap = true,
-                            maxLines = 4
+                        AsyncImage(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .fillMaxHeight(0.5f)
+                                .alpha(0.88f),
+                            model = imageUrl,
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop
                         )
-                    }
-                    FudgeTvText(
-                        titleText = song.description,
-                        modifier = Modifier.padding(bottom = 28.dp),
-                        type = FudgeTvTextTypeEnum.BODY_SMALL,
-                        textColor = Color.LightGray,
-                        overflow = TextOverflow.Ellipsis,
-                        softWrap = true,
-                        maxLines = 3
-                    )
-                    FudgeTvButton(
-                        modifier = Modifier
-                            .focusRequester(focusRequester)
-                            .fillMaxWidth()
-                            .padding(bottom = 12.dp),
-                        type = FudgeTvButtonTypeEnum.MEDIUM,
-                        style = FudgeTvButtonStyleTypeEnum.NORMAL,
-                        textRes = R.string.favorites_open_song_detail
-                    ) {
-                        onOpenSongDetail(song.id)
-                    }
-                    FudgeTvButton(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(bottom = 12.dp),
-                        type = FudgeTvButtonTypeEnum.MEDIUM,
-                        style = FudgeTvButtonStyleTypeEnum.INVERSE,
-                        textRes = R.string.more_options_remove_from_favorites_button_text
-                    ) {
-                        onRemoveFromFavorites(song.id)
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(surfaceVariant.copy(alpha = 0.6f))
+                                .padding(horizontal = 24.dp),
+                            horizontalAlignment = Alignment.Start
+                        ) {
+                            Spacer(modifier = Modifier.fillMaxHeight(0.42f))
+                            FudgeTvText(
+                                modifier = Modifier.padding(bottom = 8.dp),
+                                type = FudgeTvTextTypeEnum.HEADLINE_SMALL,
+                                textColor = onSurface,
+                                titleText = title,
+                                singleLine = true,
+                                overflow = TextOverflow.Ellipsis,
+                            )
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(bottom = 8.dp),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.Start
+                            ) {
+                                FudgeTvText(
+                                    modifier = Modifier,
+                                    type = FudgeTvTextTypeEnum.LABEL_MEDIUM,
+                                    titleText = formatSubtitle(),
+                                    textColor = onSurface,
+                                    overflow = TextOverflow.Ellipsis,
+                                    softWrap = true,
+                                    maxLines = 4
+                                )
+                            }
+                            FudgeTvText(
+                                titleText = description,
+                                modifier = Modifier.padding(bottom = 28.dp),
+                                type = FudgeTvTextTypeEnum.BODY_SMALL,
+                                textColor = onSurface,
+                                overflow = TextOverflow.Ellipsis,
+                                softWrap = true,
+                                maxLines = 3
+                            )
+                            FudgeTvButton(
+                                modifier = Modifier
+                                    .focusRequester(focusRequester)
+                                    .fillMaxWidth()
+                                    .padding(bottom = 12.dp),
+                                type = FudgeTvButtonTypeEnum.MEDIUM,
+                                style = FudgeTvButtonStyleTypeEnum.NORMAL,
+                                textRes = R.string.favorites_open_song_detail
+                            ) {
+                                onOpenSongDetail(id)
+                            }
+                            FudgeTvButton(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(bottom = 12.dp),
+                                type = FudgeTvButtonTypeEnum.MEDIUM,
+                                style = FudgeTvButtonStyleTypeEnum.TRANSPARENT,
+                                textRes = R.string.more_options_remove_from_favorites_button_text
+                            ) {
+                                onRemoveFromFavorites(id)
+                            }
+                        }
                     }
                 }
             }
